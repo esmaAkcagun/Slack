30cfc75bb37adfd7b03f88f28c8c283ab333fe15
esmaAkcagun <62145736+esmaAkcagun@users.noreply.github.com>
2020-05-12 18:23:45 +0200

    Update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -3,5 +3,7 @@ package day_1;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Login functionality");
+        
+        //i will try fetch & merge
     }
 }
