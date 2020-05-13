9c4321c25eb8981cabbfd631d4af67d7b16017b0
esmaAkcagun <esmaakcagunn@gmail.com>
2020-05-13 16:49:15 +0200

    update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -2,20 +2,13 @@ package day_1;
 
 public class Login {
     public static void main(String[] args) {
+
         System.out.println("Login functionality");
         
         //i will try fetch & merge
 
-
-
         System.out.println("inteliJ line 10");
 
-        
-        
-        
-        
-        
-        
         //this line is added from gitHub
 
     }
