0e790831132514fa5079848e4b6149e6c7b90d99
esmaAkcagun <esmaakcagunn@gmail.com>
2020-05-13 17:00:17 +0200

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -5,5 +5,11 @@ public class CreateChannel {
     public static void main(String[] args) {
 
         System.out.println("this is create channel");
+
+        int x=0;
+        if (x==0){
+            System.out.println("equal");
+        }
+
     }
 }
